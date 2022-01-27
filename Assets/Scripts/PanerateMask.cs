b289d6aa4fd20e9e3bfb13bb906d0ceae30a2f6b
yaojianzhou <terrynoya@gmail.com>
2022-01-27 15:37:45 +0800

    update test case

--- Assets/Scripts/PanerateMask.cs
+++ Assets/Scripts/PanerateMask.cs
@@ -1,36 +1,16 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class PanerateMask : MonoBehaviour,IPointerClickHandler
 {
     public List<GameObject> Targets;
 
-    public Button Btn1;
-    public Button Btn2;
-
-    private void Awake()
-    {
-        Btn1.onClick.AddListener(OnBtn1Click);
-        Btn2.onClick.AddListener(OnBtn2Click);
-    }
-
-    private void OnBtn1Click()
-    {
-        Debug.Log("btn1 clicked!!");
-    }
-    
-    private void OnBtn2Click()
-    {
-        Debug.Log("btn2 clicked!!");
-    }
-
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("mask click!!");
-        //OnlyBtn1WillTrigger(eventData);
-        BothWillTrigger(eventData);
+        OnlyBtn1WillTrigger(eventData);
+        //BothWillTrigger(eventData);
     }
 
     private void OnlyBtn1WillTrigger(PointerEventData eventData)
---
+++ Assets/Scripts/TestMain.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class TestMain:MonoBehaviour
+    {
+        public Button Btn1;
+        public Button Btn2;
+        public Button Btn3;
+        
+        private void Awake()
+        {
+            Btn1.onClick.AddListener(OnBtn1Click);
+            Btn2.onClick.AddListener(OnBtn2Click);
+            Btn3.onClick.AddListener(OnBtn3Click);
+        }
+
+        private void OnBtn1Click()
+        {
+            Debug.Log("btn1 clicked!!");
+        }
+    
+        private void OnBtn2Click()
+        {
+            Debug.Log("btn2 clicked!!");
+        }
+        
+        private void OnBtn3Click()
+        {
+            Debug.Log("btn3 clicked!!");
+        }
+    }
+}
