4f3f4ef002ca6f53e85ca11f8c20aa4fa4bc5362
yaojianzhou <terrynoya@gmail.com>
2022-01-27 17:39:27 +0800

    add ignore

--- Assets/Scripts/PanerateMask.cs
+++ Assets/Scripts/PanerateMask.cs
@@ -9,8 +9,9 @@ public class PanerateMask : MonoBehaviour,IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("mask click!!");
-        OnlyBtn1WillTrigger(eventData);
+        //OnlyBtn1WillTrigger(eventData);
         //BothWillTrigger(eventData);
+        Raycast(eventData);
     }
 
     private void OnlyBtn1WillTrigger(PointerEventData eventData)
@@ -35,6 +36,25 @@ public class PanerateMask : MonoBehaviour,IPointerClickHandler
             {
                 ExecuteEvents.ExecuteHierarchy(target, eventData, ExecuteEvents.pointerClickHandler);
             }    
+        }
+    }
+
+    
+    private readonly List<RaycastResult> _rawRaycastResults = new List<RaycastResult>();
+    
+    private void Raycast(PointerEventData eventData)
+    {
+        _rawRaycastResults.Clear();
+        EventSystem.current.RaycastAll(eventData, _rawRaycastResults);
+        foreach (var rlt in _rawRaycastResults)
+        {
+            Debug.Log(rlt.gameObject);
+            if (rlt.gameObject.GetComponent<IgnoreEventRaycast>())
+            {
+                continue;
+            }
+            ExecuteEvents.Execute(rlt.gameObject, eventData, ExecuteEvents.pointerClickHandler);
+            //ExecuteEvents.ExecuteHierarchy(rlt.gameObject, eventData, ExecuteEvents.pointerClickHandler);
         }
     }
 }
